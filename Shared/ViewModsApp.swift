46b5fa9b7c1ba7c538ed4036c1ee76c930f37331
Bosunwill <54271663+Bosunwill@users.noreply.github.com>
2021-03-03 09:05:46 -0600

    Initial Commit

---
+++ Shared/ViewModsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModsApp.swift
+//  Shared
+//
+//  Created by Gui Ndikum on 3/3/21.
+//
+
+import SwiftUI
+
+@main
+struct ViewModsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
